3befd74da536e0c25891055e4b219f38b3679d6f
Oleksii Kozhhyn <suburbakskin@gmail.com>
2019-10-14 22:24:50 +0300

    Add comments

--- crawler_vh.rb
+++ crawler_vh.rb
@@ -1,11 +1,15 @@
+# ИЗВЛЕЧЕНИЕ ИНФОРМАЦИИ ИЗ МНОЖЕСТВА WEB-СТРАНИЦ
+
 require "net/http"
 require "uri"
 
+# получение ответа от целевой страницы
 def get_responce(id)
 	uri = URI.parse("https://allkharkov.ua/reference/firm/#{id}.html")
 	Net::HTTP.get_response(uri)
 end
 
+# извлечение информации об объекте в хэш
 def extract_info(text)
 	a = {}
 	a[:name] = extract_name(text)
@@ -14,6 +18,7 @@ def extract_info(text)
 	return a
 end
 
+# замена эскейп последовательностей HTML соответствующими символами
 def unescape_html(text)
 	text.gsub(/&\w*;/) do |item|
 		case item
@@ -31,26 +36,33 @@ def unescape_html(text)
 	end
 end
 
+# извлечение имени
 def extract_name(text)
 	text =~ /<div class="firm_title"> *?<h1>(.*?)<\/h1>/ ? unescape_html(Regexp.last_match[1]) : nil
 end
 
+# извлечение почты
 def extract_email(text)
 	text =~ /E-mail:<\/div> *?<div class="info_value">(.*?@.*?\..*?)<\/div>/ ? Regexp.last_match[1].strip : nil
 end
 
+# извлечение рубрики
 def extract_activity(text)
 	text =~ /#{"Рубрики".force_encoding('ASCII-8BIT')}:<\/div> *?<div class="info_value">(.*?)[,<]/ ? Regexp.last_match[1] : nil
 end
 
+# стартовый идентификатор страницы
 start_id = 700
+# сколько страниц следует рассмотреть
 step = 100
 
 end_id = start_id + step
+# результат сохраняется в поддиректорию /results
 Dir.mkdir('results') unless Dir.exist?('results')
 input = File.open("results/res-vh-#{start_id}-#{end_id}.txt", "w")
 (start_id..end_id).each do |id|
 	responce = get_responce(id)
+	# если страница существует, она анализируется
 	if responce.code == '200'
 		firm_info = extract_info(responce.body)
 		input.puts "#{id};#{firm_info[:name]};#{firm_info[:email]};#{firm_info[:activity]}"
